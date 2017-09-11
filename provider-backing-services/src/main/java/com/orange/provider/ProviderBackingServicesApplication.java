f1eec94c6a90db9a97cba60fefee6e262d3caefa
Aymen HOUISSA <mohamedaymen.houissa@orange.com>
2017-09-11 18:14:13 +0100

    add circuit breaker example

---
+++ provider-backing-services/src/main/java/com/orange/provider/ProviderBackingServicesApplication.java
@@ -0,0 +1,12 @@
+package com.orange.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderBackingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderBackingServicesApplication.class, args);
+	}
+}
---
+++ provider-backing-services/src/test/java/com/orange/provider/ProviderBackingServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orange.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProviderBackingServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
